907d012274c6ed64e2e05dfbb5e24511fa1b0ec8
Murilo Missura <mmissura@gmail.com>
2023-07-03 19:42:42 -0300

    abstraindo funcao getAlbuns

---
+++ src/pages/Home/actions/index.jsx
@@ -0,0 +1,8 @@
+import { api } from '../../../Infra/axios';
+
+export const getAlbuns = async () => {
+  try {
+    const resposta = await api.get('/metallica');
+    return resposta;
+  } catch (error) {}
+};

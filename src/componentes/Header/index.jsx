edf22c55723bfb41b0b5e618ac0b71be19928b38
Murilo Missura <mmissura@gmail.com>
2023-07-11 19:00:39 -0300

    ajustando header

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -4,11 +4,11 @@ import logoMetallica from '../../assets/Images/logo.png';
 export const Header = () => {
   return (
     <header className="w-full bg-black p-4">
-      <div className="w-3/4 flex justify-between items-center m-auto">
+      <div className="lg:w-3/4 md:w-3/4 w-full flex justify-between items-center m-auto">
         <Link to="/">
           <img className="w-40" src={logoMetallica} alt="" />
         </Link>
-        <h1 className="text-gray-400 font-extrabold text-2xl italic">
+        <h1 className="text-gray-400 font-extrabold lg:text-2xl md:text-lg text-sm italic">
           ...And Metallica For All
         </h1>
       </div>
